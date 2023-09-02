f2a3bf6fa2a33df6365eb74a0bec4df092883af6
Juan Ignacio Marchetto <juanignaciomarchetto@gmail.com>
2023-09-02 13:34:01 -0300

    version2/9/2023-1

---
+++ passimages/passimageslist.js
@@ -0,0 +1,21 @@
+const images = [
+    '/images/cachorros.jpg',
+    '/images/capibara.jpg',
+    '/images/cerdo.jpg',
+    '/images/cuerpospin.jpg',
+    '/images/llama.jpg',
+    '/images/orangutan.avif',
+    '/images/perroblanco.jpg',
+    '/images/tigre-caminando.jpg',
+    '/images/vaca.jpg',
+    '/images/zebra.jpg',
+    '/images/zorro.jpg',
+    '/images/tortuga.jpg',
+    '/images/ardilla.jfif',
+    '/images/caballo.jfif',
+    '/images/leon.jfif',
+    '/images/mapache.jfif',
+    '/images/perezoso.jfif',
+
+
+  ];
