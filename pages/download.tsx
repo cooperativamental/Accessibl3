c86d50645972e050de3f0002dad870cbe633bc7c
JuanMarchetto <juanpatriciomarchetto@gmail.com>
2023-10-10 15:58:03 -0300

    adding functionallity

--- pages/download.tsx
+++ pages/download.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Keypair, PublicKey } from "@solana/web3.js";
+import { useEffect, useState } from "react";
+import * as bip39 from "bip39";
 
 export default function Home() {
+
+  const [keys, setKeys] = useState<(string)[]>([]);
+
+  useEffect(() => {
+    let keys = [];
+    try { keys = JSON.parse(localStorage.getItem('mnemonic')??"");
+    } catch (error) { }
+    if (keys.length > 0) {
+     setKeys(keys);
+    }else{      
+      const key = bip39?.generateMnemonic();
+
+      const keyspairs = [key]
+      localStorage.setItem('mnemonic', JSON.stringify(keyspairs));
+      setKeys(keyspairs);
+      
+    }
+  }, []);
+
   const router = useRouter();
 
   return (
@@ -18,12 +38,31 @@ export default function Home() {
           <input
             style={{ marginBottom: "20px", marginTop: "15px" }}
             type="text"
-            placeholder="a34yr47g4488t47ty..."
+            placeholder="..."
+            readOnly
+            value={
+              Keypair.fromSeed((bip39.mnemonicToSeedSync(keys[0], "")).slice(0, 32)).publicKey.toBase58()
+            }
           />
           <br />
           <button
             className="download"
             style={{ marginBottom: "20px", marginTop: "15px" }}
+            onClick={() => {
+              // return a wallet.json file with the keypairs secretKey
+              const element = document.createElement("a");
+              const seed = bip39.mnemonicToSeedSync(keys[0], ""); // (mnemonic, password)
+      const keypair = Keypair.fromSeed(seed.slice(0, 32));
+      console.log(`${keypair.publicKey.toBase58()}`); // 5ZWj7a1f8tWkjBESHKgrLmXshuXxqeY9SYcfbshpAqPG
+      console.log(`Private key: [${keypair.secretKey.toString()}]`);
+              const file = new Blob([`[${keypair.secretKey.toString()}]`], {
+                type: "text/plain;charset=utf-8",
+              });
+              element.href = URL.createObjectURL(file);
+              element.download = "wallet.json";
+              document.body.appendChild(element);
+              element.click();
+            }}
           >
             Download <img src="../relampago.png" alt="" />
           </button>
