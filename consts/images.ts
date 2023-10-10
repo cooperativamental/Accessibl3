9cf2f0396a140e2d629a6d1ed5bf8ce68d0066e1
Marche <juanpatriciomarchetto@gmail.com>
2023-10-10 17:57:45 -0300

    Piertotum locomotor (#3)

---
+++ consts/images.ts
@@ -0,0 +1,14 @@
+const images = [
+    "/insectos/honey-bee-water-buckfast-59829.jpeg",
+           "/acsesorios/paraguas.jpg",
+           "/comida/frutilla.jpg",
+           "/deportes/golf.jpg",
+          "/comida/cebolla.jpg",
+           "/iconos/wifi.png",
+           "/digujos/capitan america.jpeg",
+           "/animales/mamut.jfif",
+           "/astros/earth-blue-planet-globe-planet-87651.jpeg",
+           "/cocina/palillos.jfif",
+  ];
+
+export default images
--- pages/download.tsx
+++ pages/download.tsx
@@ -1,10 +1,30 @@
 import Image from "next/image";
-import { Inter } from "next/font/google";
 import { useRouter } from "next/router";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Keypair } from "@solana/web3.js";
+import { useEffect, useState } from "react";
+import * as bip39 from "bip39";
+import words from "@/consts/words";
+import images from "@/consts/images";
 
 export default function Home() {
+  const [keys, setKeys] = useState<(string)[]>([]);
+
+  useEffect(() => {
+    let keys = [];
+    try {
+      keys = JSON.parse(localStorage.getItem('mnemonic') ?? "");
+    } catch (error) { }
+    if (keys.length > 0) {
+      setKeys(keys);
+    } else {
+      const key = bip39?.generateMnemonic();
+      const keyspairs = [key]
+      localStorage.setItem('mnemonic', JSON.stringify(keyspairs));
+      setKeys(keyspairs);
+
+    }
+  }, []);
+
   const router = useRouter();
 
   return (
@@ -18,69 +38,64 @@ export default function Home() {
           <input
             style={{ marginBottom: "20px", marginTop: "15px" }}
             type="text"
-            placeholder="a34yr47g4488t47ty..."
+            placeholder="..."
+            readOnly
+            value={
+              Keypair.fromSeed((bip39.mnemonicToSeedSync(keys[0], "")).slice(0, 32)).publicKey.toBase58()
+            }
           />
           <br />
           <button
             className="download"
             style={{ marginBottom: "20px", marginTop: "15px" }}
+            onClick={() => {
+              // return a wallet.json file with the keypairs secretKey
+              const element = document.createElement("a");
+              const seed = bip39.mnemonicToSeedSync(keys[0], ""); // (mnemonic, password)
+              const keypair = Keypair.fromSeed(seed.slice(0, 32));
+              const file = new Blob([`[${keypair.secretKey.toString()}]`], {
+                type: "text/plain;charset=utf-8",
+              });
+              element.href = URL.createObjectURL(file);
+              element.download = "wallet.json";
+              document.body.appendChild(element);
+              element.click();
+            }}
           >
             Download <img src="../relampago.png" alt="" />
           </button>
         </div>
         <br />
-        <div className="image-container" style={{ marginTop: "15px" }}>
-          <div className="image">
-            <img src="/deportes/atletismo.jpg" alt="Imagen 1" />
-          </div>
-          <div className="image">
-            <img src="/deportes/ajedres.jpg" alt="Imagen 2" />
-          </div>
-          <div className="image">
-            <img src="/deportes/bowling.jpg" alt="Imagen 3" />
-          </div>
-        </div>
-        <div className="image-container">
-          <div className="image">
-            <img src="/deportes/baseball.jpg" alt="Imagen 1" />
-          </div>
-          <div className="image">
-            <img
-              src="/deportes/equitacion.jpg"
-              alt="Imagen 2"
-            />
-          </div>
-          <div className="image">
-            <img src="/deportes/ciclismo.jpg" alt="Imagen 3" />
-          </div>
-        </div>
-        <div className="image-container">
-          <div className="image">
-            <img src="/deportes/boxeo.jpg" alt="Imagen 1" />
-          </div>
-          <div className="image">
-            <img src="/deportes/escalada.jpg" alt="Imagen 2" />
-          </div>
-          <div className="image">
-            <img
-              src="/deportes/pelota hokey.jpg"
-              alt="Imagen 3"
-            />
-          </div>
-        </div>
-        <div className="image-container">
-          <div className="image">
-            <img src="/deportes/golf.jpg" alt="Imagen 1" />
-          </div>
-          <div className="image">
-            <img src="/deportes/martillo.jpg" alt="Imagen 2" />
-          </div>
-          <div className="image">
-            <img src="/deportes/rugby.jpg" alt="Imagen 3" />
-          </div>
+        <div className="grid grid-rows-3 grid-flow-col gap-4">
+
+        {keys[0]?.split(" ")?.map((word) => {
+          const index = words.findIndex((w) => w === word);
+          if (images.length > index) {
+            return (
+              <Image
+                key={word}
+                src={images[index]}
+                alt={word}
+                width={200}
+                height={200}
+                className=""
+              />
+            );
+          } else {
+            return (
+              <Image
+                key={word}
+                src={images[0]}
+                alt={word}
+                width={200}
+                height={200}
+                className=""
+              />
+            );
+          } 
+        })}
         </div>
       </div>
-      <div className="flame"></div>
     </main>
   );
 }
