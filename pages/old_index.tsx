db236a678eba77c64c9ea4fddfd764bbb273ca07
Marche <juanpatriciomarchetto@gmail.com>
2023-10-11 15:23:00 -0300

    Quick Quotes Quill (#5)

---
+++ pages/old_index.tsx
@@ -0,0 +1,50 @@
+import Image from "next/image";
+import { useRouter } from "next/router";
+
+export default function Home() {
+  const router = useRouter();
+
+  return (
+    <main>
+      <div className="container">
+        <br />
+        <img className="logo" src="/ACC_EASE_sombra.png" alt="" />
+        <br />
+        <br />
+        <h2
+          className="text-center text-[orangered]"
+          style={{
+            fontSize: "larger",
+            marginBottom: "75px",
+          }}
+        >
+          Chose your type of identity confirmation
+        </h2>
+      </div>
+      <br />
+        <div
+          className="image-container"
+          style={{ display: "grid", justifyItems: "center" }}
+        >
+          <button
+            id="PassWord"
+            className="flex items-center p-8 h-20 justify-items-center bg-green-600"
+            onClick={() => {
+              alert("Will add this feature soon!");
+            }}
+          >
+            PassWord
+          </button>
+          <button
+            id="PassImages"
+            className="flex items-center p-8 h-20 justify-items-center bg-green-600"
+            onClick={() => {
+              router.push("/chose");
+            }}
+          >
+            PassImages
+          </button>
+        </div>
+    </main>
+  );
+}
